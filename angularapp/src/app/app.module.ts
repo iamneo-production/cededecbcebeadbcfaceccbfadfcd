314b2247f38188ec529422ce540f0fef0675c6df
Iamneo <techadmin@iamneo.ai>
2023-08-02 09:26:09 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ForexConverterPipe } from './forex-conversion.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForexConverterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
